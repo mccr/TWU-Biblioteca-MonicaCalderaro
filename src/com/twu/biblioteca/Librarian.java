4c8c9f9f6571720e116ef37f67c38b0635907276
Monica Calderaro <mcalderarorey@gmail.com>
2018-05-15 16:42:28 +0200

    librarian can checkout books

--- src/com/twu/biblioteca/Librarian.java
+++ src/com/twu/biblioteca/Librarian.java
@@ -2,6 +2,12 @@ package com.twu.biblioteca;
 
 public class Librarian {
     public void checkout(Book book) {
-        System.out.println("Thank you! Enjoy the book");
+        if (book.isAvailable == true) {
+            book.isAvailable = false;
+            System.out.println("Thank you! Enjoy the book");
+        } else {
+            System.out.println("That book is not available.");
+        }
+
     }
 }
--- test/com/twu/biblioteca/LibrarianTest.java
+++ test/com/twu/biblioteca/LibrarianTest.java
@@ -20,12 +20,33 @@ public class LibrarianTest {
         System.setOut(System.out);
     }
 
+    Librarian librarian;
+    Book book;
+    @Before
+    public void setUp() throws Exception {
+        librarian = new Librarian();
+        book = new Book("book", "author", 2000);
+    }
+
     @Test
-    public void shouldBeAbleToCheckoutBook() {
-        Librarian librarian = new Librarian();
-        Book book = new Book("book", "author", 2000);
+    public void shouldBeAbleToCheckoutBookAndMarkItUnavailableAndShowSuccessMessage() {
+
         librarian.checkout(book);
 
         assertEquals("Thank you! Enjoy the book", outContent.toString().trim());
+        assertEquals(false, book.isAvailable);
+    }
+
+    @Test
+    public void shouldBeAbleToShowAUnsuccessfulMessage() {
+        book.isAvailable = false;
+
+        librarian.checkout(book);
+
+        assertEquals("That book is not available.", outContent.toString().trim());
+    }
+
+    @Test
+    public void shouldBeAbleToReturnBooks() {
     }
 }
